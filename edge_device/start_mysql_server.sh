bbaab485f33b3efa8bc27a68e040ed10654146fb
Your Name <you@example.com>
2018-11-29 09:34:48 -0600

    fixed edge stuff'

--- edge_device/start_mysql_server.sh
+++ edge_device/start_mysql_server.sh
@@ -1,4 +1,3 @@
-docker run --name mysql57 -d --restart unless-stopped -p 127.0.0.1:3306:3306 ibex/debian-mysql-server-5.7
-echo "Run Command to enter mysql database: docker exec -it mysql57 "mysql" -uroot"
-echo "CREATE USER 'root'@'192.168.%' IDENTIFIED by 'cloud123';"
-echo "CREATE USER 'root'@'172.%' IDENTIFIED by 'cloud123';"
+docker run --name mysql57 -d --restart unless-stopped -p 3306:3306 ibex/debian-mysql-server-5.7
+echo "Run Command to enter mysql database: docker exec -it mysql57 "mysql" -u root"
+echo "CREATE USER 'root'@'%' IDENTIFIED by 'cloud123';"
